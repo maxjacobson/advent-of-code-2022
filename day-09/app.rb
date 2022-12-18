8e8347f8922a12860e19c7f00df7016ba7829c3a
Max Jacobson <max@hardscrabble.net>
2022-12-17 19:37:45 -0500

    Add day 9
    needed to refactor the whole thing sob

--- day-09/app.rb
+++ day-09/app.rb
@@ -4,107 +4,175 @@ require 'irb'
 Instruction = Struct.new(:direction, :amount, keyword_init: true)
 Pos = Struct.new(:x, :y)
 
-class Rope
-  attr_reader :head, :tail
-
-  def initialize(head:, tail:)
-    @head = head
-    @tail = tail
+class PrintOut
+  def initialize(rope)
+    @rope = rope
   end
 
-  def move(direction:)
-    new_head = new_pos_in_direction(head, direction)
+  def print
+    positions = []
 
-    new_tail =
-      if touching?(new_head, tail)
-        tail
-      elsif new_head.x == tail.x || new_head.y == tail.y
-        new_pos_in_direction(tail, direction)
-      else
-        x_delta = new_head.x > tail.x ? 1 : -1
-        y_delta = new_head.y > tail.y ? 1 : -1
+    r = @rope
+    loop do
+      break if r.nil?
+
+      positions.push(r.position)
+      r = r.next_knot
+    end
+
+    grid = Array.new(40).map {
+      Array.new(40).map {
+        '.'
+      }
+    }
+
+    starting_pos = Pos.new(
+      11,
+      15,
+    )
+
+    positions.each.with_index do |pos, index|
+      grid[starting_pos.y + pos.y][starting_pos.x + pos.x] = index.to_s
+    end
+
+    puts
+    puts grid.reverse.map { |row| row.join("") }.join("\n")
+    puts
+  end
+end
+
+class Knot
+  attr_reader :position, :next_knot
+
+  def initialize(position:, next_knot:)
+    @position = position
+    @next_knot = next_knot
+
+    @all_positions_ever = [position]
+  end
+
+  def print_out
+    # PrintOut.new(self).print
+  end
+
+  def move_to(new_position)
+    @position = new_position
+    @all_positions_ever.push(new_position)
+
+    if next_knot
+      next_knot.follow_toward(new_position)
+    end
+  end
+
+  def final_knot
+    if next_knot
+      next_knot.final_knot
+    else
+      self
+    end
+  end
+
+  def unique_positions_count
+    @all_positions_ever.uniq.count
+  end
+
+  protected
+
+  def follow_toward(head_position)
+    if touching?(head_position, position)
+      # no-op
+    elsif head_position.x == position.x
+      move_to(
         Pos.new(
-          tail.x + x_delta,
-          tail.y + y_delta,
+          head_position.x,
+          head_position.y > position.y ? position.y + 1 : position.y - 1
         )
-      end
-
-    Rope.new(head: new_head, tail: new_tail)
+      )
+    elsif head_position.y == position.y
+      move_to(
+        Pos.new(
+          head_position.x > position.x ? position.x + 1 : position.x - 1,
+          head_position.y
+        )
+      )
+    else
+      x_delta = head_position.x > position.x ? 1 : -1
+      y_delta = head_position.y > position.y ? 1 : -1
+      pos = Pos.new(
+        position.x + x_delta,
+        position.y + y_delta,
+      )
+      move_to(
+        pos,
+      )
+    end
   end
-
-  private
 
   def touching?(p1, p2)
     (p1.x - p2.x).abs <= 1 && (p1.y - p2.y).abs <= 1
   end
-
-  def new_pos_in_direction(pos, direction)
-      case direction
-      when "R"
-        Pos.new(pos.x + 1, pos.y)
-      when "L"
-        Pos.new(pos.x - 1, pos.y)
-      when "U"
-        Pos.new(pos.x, pos.y + 1)
-      when "D"
-        Pos.new(pos.x, pos.y - 1)
-      else
-        raise
-      end
-  end
 end
 
 class RopePath
-  def initialize(filename)
+  def initialize(filename, knots:)
     @instructions = File.read(filename).lines.map do |line|
       direction, amount = line.chomp.split(" ")
       Instruction.new(direction: direction, amount: amount.to_i)
     end
+    @rope = knots.times.inject(nil) do |acc, _|
+      Knot.new(position: Pos.new(0, 0), next_knot: acc)
+    end
   end
 
   def tail_positions_count
-    complete_path.map(&:tail).uniq.count
-  end
+    return @tail_positions_count if defined?(@tail_positions_count)
 
-  private
-
-  def complete_path
-    return @complete_path if defined?(@complete_path)
-
-    @complete_path = [
-      Rope.new(
-        head: Pos.new(0, 0),
-        tail: Pos.new(0, 0),
-      )
-    ]
-
+    @rope.print_out
     @instructions.each do |instruction|
       instruction.amount.times do
-        @complete_path.push @complete_path.last.move(direction: instruction.direction)
+        pos = @rope.position
+
+        new_pos =
+          case instruction.direction
+          when "R"
+            Pos.new(@rope.position.x + 1, @rope.position.y)
+          when "L"
+            Pos.new(@rope.position.x - 1, @rope.position.y)
+          when "U"
+            Pos.new(@rope.position.x, @rope.position.y + 1)
+          when "D"
+            Pos.new(@rope.position.x, @rope.position.y - 1)
+          else
+            raise
+          end
+
+        @rope.move_to(new_pos)
+        @rope.print_out
       end
     end
 
-
-    @complete_path
+    @tail_positions_count = @rope.final_knot.unique_positions_count
   end
 end
 
 class RopeTest < Minitest::Test
   def test_example
-    rope = RopePath.new("./example.txt")
+    rope = RopePath.new("./example.txt", knots: 2)
     assert_equal 13, rope.tail_positions_count
   end
 
   def test_actual
-    rope = RopePath.new("./input.txt")
+    rope = RopePath.new("./input.txt", knots: 2)
     assert_equal 6498, rope.tail_positions_count
   end
 
   def test_example_part_two
-    skip "not yet"
+    rope = RopePath.new("./larger-example.txt", knots: 10)
+    assert_equal 36, rope.tail_positions_count
   end
 
   def test_actual_part_two
-    skip "not yet"
+    rope = RopePath.new("./input.txt", knots: 10)
+    assert_equal 2531, rope.tail_positions_count
   end
 end
